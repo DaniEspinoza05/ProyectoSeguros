234b40665ac40ac4caa4de3ef4d26d34abccd3ed
Sebastián Merino Rodríguez <98624720+Semero34@users.noreply.github.com>
2024-11-16 16:15:54 -0600

    Dashboard actualizado

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,11 @@
 <?php
-echo "<h1>Bienvenido al Dashboard!</h1>";
+session_start();
+if (!isset($_SESSION['username'])) {
+    // Redirigir al login si no hay sesión activa
+    header("Location: login.php");
+    exit();
+}
 ?>
+
+<!-- Contenido del dashboard -->
+<h1>Bienvenido al Dashboard, <?php echo $_SESSION['username']; ?>!</h1>
