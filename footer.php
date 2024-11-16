d78e3515bf7439a0b1020e261e126a9a72d104b1
Sebastián Merino Rodríguez <98624720+Semero34@users.noreply.github.com>
2024-11-16 16:17:28 -0600

    footer creado

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <p>&copy; 2024 SaveMe - Todos los derechos reservados</p>
+    <p><a href="privacidad.php">Política de privacidad</a> | <a href="terminos.php">Términos y condiciones</a></p>
+    <p>Teléfono: +123 456 789 | Email: contacto@save-me.com</p>
+    <p>
+        <a href="https://www.facebook.com/SaveMe" target="_blank">Facebook</a> |
+        <a href="https://twitter.com/SaveMe" target="_blank">Twitter</a> |
+        <a href="https://www.instagram.com/SaveMe" target="_blank">Instagram</a>
+    </p>
+</footer>
